b7881b638febe77e0f2bf9cbe14dc42198ec3ff6
Rob Orndoff <rorndoff@gmail.com>
2019-07-18 08:29:08 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monte_Carlo_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
